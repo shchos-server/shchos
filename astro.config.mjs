954766be51077bc6587c0165ca1125424553a511
Andrii Avramenko <andreitvmm228@gmail.com>
2026-09-03 19:21:41 +0100

    New animated favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://andrii-avramenko.github.io",
+  site: "https://shchos-server.github.io",
 
   i18n: {
     locales: ["en", "uk"],
